fd82d807a2b5ec9aa3d0963ae084663c1d1d6f9e
Pedro-Henriquebg7 <85884290+Pedro-Henriquebg7@users.noreply.github.com>
2023-11-29 07:55:23 -0300

    Add files via upload

--- django_test/mytestsite/locallibrary/locallibrary/catalog/urls.py
+++ django_test/mytestsite/locallibrary/locallibrary/catalog/urls.py
@@ -1,7 +1,17 @@
+from .models import Book
 from django.urls import path
 from catalog import views
 
-
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
